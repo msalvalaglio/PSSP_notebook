cb10d4dfaf34756aecbf983f0b2a452ca0910858
Matteo <matteo@matteos-mbp.home>
2021-02-06 18:45:21 +0000

    notes update

--- _build/jupyter_execute/04-Process_layouts.py
+++ _build/jupyter_execute/04-Process_layouts.py
@@ -8,63 +8,61 @@ __Problem Statement__
 
 500 l of fruit juice are concentrated from an initial solid-residue content of 0.05 kg/l to a solid-residue content of 0.2 kg/l through a batch microfiltration process. The total area of the membrane is of 20 $\mathrm{m^2}$ and the flux of pure water through the membrane is captured by the empirical expression: $J=BC^{-1} [m/h]$ with $B=0.1$ where C is the solid residue concentration in kg/l. Compute the process time necessary to reach the target concentration.
 
-from IPython.display import Image
-fig = Image(filename=('./batch.png'), width=300, height=200)
-fig
+<img src="batch.png" alt="Drawing" style="width: 300px;"/>
 
 __Solution trace__
 
 The global differential material balance (volume basis) reads: 
 
-\begin{equation}
+$$
 \frac{dV}{dt}=-J\,A
-\end{equation}
+$$(eq1)
 
 where $V$ is the volume in the concentrate loop, $J$ the permeate flux per unit area and $A$ the total membrane area. 
 The permeate flux is defined by the expression: 
-\begin{equation}
+$$
 J=BC^{-1}
-\end{equation}
+$$(eq2)
 with $B=0.1$. 
 
 Since the solid residue never leaves the retentate loop,  the differential material balance on the solid residue reads: 
-\begin{equation}
+$$
 \frac{dm}{dt}=0 
-\end{equation}
+$$(eq3)
 
 Which means that the total mass of solid residue is constant and equal to its initial mass: 
-\begin{equation}
+$$
 m=m_0=V_0C_0 
-\end{equation}
+$$(eq4)
 
 The global differential material balance can thus be rewritten as: 
-\begin{equation}
+$$
 \frac{dV}{dt}=-J\,A=-\frac{B}{V_0C_0 }AV
-\end{equation}
+$$(eq5)
 
 \newpage
 
 In this simple case it can be integrated analytically to compute the volume as a function of time: 
-\begin{equation}
+$$
 \int_{V_0}^{V^{\prime}}\frac{dV}{V}=\int_0^{t^{\prime}}-\frac{B}{V_0C_0 }Adt
-\end{equation}
-\begin{equation}
+$$(eq6)
+$$
 \int_{V_0}^{V^{\prime}}\frac{dV}{V}=\left[\ln{V}\right]^{V^{\prime}}_{V_0}=\ln\left(\frac{V^\prime}{V_0}\right)
-\end{equation}
-\begin{equation}
+$$(eq7)
+$$
 \int_0^{t^{\prime}}-\frac{B}{V_0C_0 }Adt=-\frac{B}{V_0C_0 }At^\prime
-\end{equation}
+$$(eq8)
 
 Putting together both sides of the equation and substituting $V^\prime$ with $V$ for the sake of simplicity in the notation we get: 
-\begin{equation}
+$$
 V(t)=V_0e^{-\frac{B}{V_0C_0 }At}
-\end{equation}
+$$(eq9)
 which describes the time dependence of the volume in the batch membrane separator. 
 
 The concentration can thus be calculated as: 
-\begin{equation}
+$$
 C(t)=\frac{m}{V(t)}=\frac{C_0V_0}{V_0}e^{\frac{B}{V_0C_0 }At}={C_0}e^{\frac{B}{V_0C_0 }At}
-\end{equation}
+$$(eq10)
 
 The process time necessary to obtain a solid residue concentration of 0.2 $[kg/l]$ is of 17.3 $[h]$
 
@@ -125,33 +123,31 @@ __Feed and Bleed:  Problem Statement__
 
 500 l of fruit juice are concentrated in 5 h of steady state operation from an initial solid-residue content of 0.05 $kg/l$ through a feed and bleed process. The total area of the membrane is of 20 $m^2$ and the flux of pure water through the membrane is captured by the empirical expression: $J=BC^{-1} [m/h]$ with $B=0.1$ where C is the solid residue concentration in kg/l. Is the steady state concentration of the retentate compatible with the specifics of 0.2 $kg/l$? 
 
-from IPython.display import Image
-fig = Image(filename=('./feedandbleed.png'), width=300, height=200)
-fig
+<img src="feedandbleed.png" alt="Drawing" style="width: 300px;"/>
 
 __Feed and Bleed: Solution trace__
 
 Also in this case the global differential material balance and the solid-residue material balance can be written as follows: 
 
-\begin{equation}
+$$
 \frac{dV}{dt}=Q_{IN}-J\,A-Q_{OUT}=0
-\end{equation}
+$$(eq11)
 
-\begin{equation}
+$$
 \frac{dm}{dt}=Q_{IN}C_{IN}-Q_{OUT}C_{OUT}=0
-\end{equation}
+$$(eq12)
 
 At steady state ${dV}/{dt}=0$ as well as ${dm}/{dt}=0$. The two ODEs become then two algebraic equations that should be solved together to compute the steady state concentration. 
 From the global material balance we get: 
 
-\begin{equation}
+$$
 Q_{OUT}=Q_{IN}-J\,A=Q_{IN}-\frac{B}{C_{OUT}}A
-\end{equation}
+$$(eq13)
 
 and then, with some manipulations: 
-\begin{equation}
+$$
 C_{OUT}=C_{IN}+\frac{AB}{Q_{IN}}
-\end{equation}
+$$(eq14)
 
 import numpy as np
 import matplotlib.pyplot as plt 
@@ -178,39 +174,37 @@ The flux of pure water through the membrane is captured by the empirical express
 
 Is it more efficient to design a single stage configuration with four units in parallel or a cascade configuration with four units in series? 
 
-from IPython.display import Image
-fig = Image(filename=('./cascade.png'), width=600, height=200)
-fig
+<img src="cascade.png" alt="Drawing" style="width: 600px;"/>
 
 __Cascade: Solution trace__
 
 Each stage can be treated like a single unit in which the feed stream corresponds to the retentate stream from the previous unit.
 We can thus define: 
-\begin{equation}
+$$
 C_{OUT,i}=C_{IN,i+1}
-\end{equation}
-\begin{equation}
+$$(eq15)
+$$
 Q_{OUT,i}=Q_{IN,i+1}
-\end{equation}
+$$(eq16)
 where the index $i$ identifies the stage. 
 
 The material balances for each stage at steady state can thus be written as: 
-\begin{equation}
+$$
 \frac{dV}{dt}=Q_{IN,i}-n_iJ(C_{IN,i+1})\,A-Q_{IN,i+1}=0
-\end{equation}
+$$(eq17)
 
-\begin{equation}
+$$
 \frac{dm}{dt}=Q_{IN,i}C_{IN}-Q_{IN,i+1}C_{IN,i+1}=0
-\end{equation}
+$$(eq18)
 where $n_i$ is the number of modules used in stage $i$.
 
 For each stage we can thus compute the steady state concentration and concentrate volumetric flow by solving sequentially the following equations:  
-\begin{equation}
+$$
 C_{IN,i+1}=C_{IN,i}+n_i\frac{AB}{Q_{IN,i}}
-\end{equation}
-\begin{equation}
+$$(eq19)
+$$
 Q_{IN,i+1}=\frac{C_{IN,i}{Q_{IN,i}}}{C_{IN,i+1}}
-\end{equation}
+$$(eq20)
 
 with $i=1...N$ with $N$ is the total number of stages. 
 
