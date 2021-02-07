29c483ab4c4cfffd2d79ddd799edf58b28d49a8d
Matteo <matteo@matteos-mbp.home>
2021-02-07 17:12:18 +0000

    notes update

---
+++ _build/jupyter_execute/Exercise_W5_2021.py
@@ -0,0 +1,18 @@
+# Week 5 - Exercise
+
+## Problem Statement
+
+A protein mixture has been separated through an ion-exchange chromatography process. The residence time of the column in non-adsorbing conditons is $t_0=1$ min.
+The chromatogram resulting from the separation is reported in the following: 
+
+<img src="./chromatogram.png" alt="Drawing" style="width: 600px;">
+
+### Determine:
+- How many components in the mixture can you identify from the chromatogram?
+- What is the selectivity of this column for each couple of components? 
+- Assuming a linear adsorption isotherm holds in all cases, what conclusions can you draw about adsorption thermodynamics of each component in the column?
+- What is the number of theoretical plates associated to the elution of each component you have identified? If the coulmn is 10 cm long, what is the height equivalent of a theoretical plate for each component of the mixture? 
+- Compute the resolution relative to each couple of components in the chromatogram. Does it depend only on the adsorption thermodynamics?
+- Comment on the separation performances of the column. Does it allow for a sharp resolution of all components?
+
+
