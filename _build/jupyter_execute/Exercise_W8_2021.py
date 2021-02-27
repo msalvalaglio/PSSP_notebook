903736193ddc1be5352d5b158c1d34db9923bd77
msalvalaglio <matteo.salvalaglio@gmail.com>
2021-02-27 02:34:37 +0000

    added exercises week 8 and 9

---
+++ _build/jupyter_execute/Exercise_W8_2021.py
@@ -0,0 +1,8 @@
+# Week 8 - Exercise
+
+## Problem Statement
+An aqueous feed of 10000 kg h$^-1$, saturated with $BaCl_2$ at 100$^o$C, enters a crystallizer that can be simulated with the MSMPR model. The slurry leaves the crystallizer at 20$^o$C with crystals of the dihydrate. The crystallizer has a volume of 2.0 $m^3$. From laboratory experiments, the crystal growth rate is essentially constant at $4.0\times{10^{-7}}$ m/s. The density of the dihydrate crystals is 3.097 g cm$^{-3}$. The density of an aqueous, saturated solution of barium chloride at 20$^o$C is 1.29 g cm$^{-3}$. The solubility of $BaCl_2$ is 58.3 g/100 g water at 100$^o$ C and 35.7 g/100 g water at 20$^o$ C. The molar mass of $BaCl_2$ is 208.27 g/mol, the the dihydrate, $BaCl_2\dot2H_2O$ has a molar mass of 244.31 g/mol. The density of the crystals is 3097 $kg/m^3$, the density of the solution in the crystallizer is 1279 $kg/m^3$ 
+
+- Determine the mass flow rate of crystals in the product stream (also known as 
+- Compute the predominant crystal size $L_D$ in mm.
+
