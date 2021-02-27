df88bbce99ba27c12276ad75567506e4cfdd2b0c
msalvalaglio <matteo.salvalaglio@gmail.com>
2021-02-27 03:09:23 +0000

    test

--- _build/jupyter_execute/Exercise_W9_2021.py
+++ _build/jupyter_execute/Exercise_W9_2021.py
@@ -4,7 +4,8 @@
 
 The cumulative particle size distribution $N(L)$ has been measured for two different MSMPR crystallization processes (A and B) and is reported in Fig. 1. Both processes are producing the same compact crystals, under different residence times and temperatures. The growth rate is known to be independent from both temperature and composition ($G=const$). The nucleation rate $J$ instead depends exponentially on the inverse of the temperature through an Arrhenius-like expression ($J=A\exp{-B/T}$,  where $A$ and $B$ are constants).
 
-<img src="./PSDs_exercise.jpg" alt="Drawing" style="width: 500px;">
+<img src="./PSDexercise.png" alt="Drawing" style="width: 500px;">
+<img src="./chromatogram.png" alt="Drawing" style="width: 600px;">
 
 - Compute and plot the number density, length, area, and normalised mass distribution for these two processes. [Download the dataset](https://raw.githubusercontent.com/msalvalaglio/PSSP_notebook/master/EX_W9_data.txt).
 - Compute the dominant length $L_D$ obtained from both processes. 
