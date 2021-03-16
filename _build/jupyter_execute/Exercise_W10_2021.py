383e3b653950c9167bc433616d8de5fcfab65ba1
msalvalaglio <matteo.salvalaglio@gmail.com>
2021-03-16 19:13:20 +0000

    Added exercise W10

--- _build/jupyter_execute/Exercise_W10_2021.py
+++ _build/jupyter_execute/Exercise_W10_2021.py
@@ -1,16 +1,26 @@
 # Week 10 - Exercise
 
-## Problem Statement 
+### Problem Statement 
 
 Consider a drying process evolving through two stages of _i_ constant drying rate, and _ii_ falling rate (first order kinetics). 
 
-During stage _i_ the drying rate per unit area is constant, and indicated with $R_c=m\times{f_c}=const$. The the critical moisture content is indicated with $w_c$, and $w_0$ indicates initial moisture content. 
+During stage _i_ the drying rate _per unit area_ is constant, and indicated with $R_c=m\times{f_c}=const$. 
 
-- Derive an expression for the time necessary to reduce the moisture content from $w_0$ to $w_c$. 
+Where: 
+- $m$ is a drying rate constant 
+- $f_c=w_c-w_e$
+- $w_c$ is the critical moisture content
+- $w_e$ is the equilibrium moisture content
+- $w_0$ is the initial moisture content
 
-During stage _ii_ the rate of drying is constant, and proportional to the free moisture content. The falling rate regime stops at the rquilibrium moisture content $w_e$. 
+During stage _ii_ the rate of drying is constant, and proportional to the free moisture content ($f=w-w_e$). The falling rate regime stops at the rquilibrium moisture content $w_e$. 
 
+### Tasks
+
+- Derive an expression for the time necessary to reduce the moisture content from $w_0$ to $w_c$.
 - Derive an expression for the time necessary to reduce the moisture content from $w_c$ to $w_e$ in the (linear) falling regime.
+- Derive an expression for the time necessary to reduce the moisture content from $w_0$ to $w_e$ in the (linear) falling regime.
+- If a system, characterised by critical moisture content of 18% and an equilibrium moisture content of 2% takes 7300 seconds to reach a final moisture content of 5% from an initial moistyure content of 20%, how long will it take, in the same conditions, to dry a system from 30% to 3%.
+- For the same system produce a plot of the drying time as a function of the kinetic constant m
 
 
-If a system, characterised by critical moisture content of 18% and an equilibrium moisture content of 2% takes 7300 seconds to reach a final moisture content of 5% from an initial moistyure content of 20%, how long will it take, in the same conditions, to dry a system from 30% to 3%. 
