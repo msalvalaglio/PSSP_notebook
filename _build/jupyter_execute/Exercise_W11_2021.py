ceb29ff9c987ce9abd089823cd53662077ef27a5
msalvalaglio <matteo.salvalaglio@gmail.com>
2021-03-22 13:30:31 +0000

    Added Week 11 exercise

---
+++ _build/jupyter_execute/Exercise_W11_2021.py
@@ -0,0 +1,12 @@
+# Week 11 - Exercise
+
+## Problem Statement
+
+Water (density 1000 kg m-3, viscosity 1 mN s m-2) is used to fluidise a bed of packed spheres characterised by a diameter of 0.5 mm and a density of 1500 kg m-3.
+
+### Tasks
+- Demonstrate that, in laminar regime, the minimum fluidization velocity is alway smaller than the terminal falling velocity. 
+- Compute the minimum fluidisation velocity and the terminal falling velocity of the bed particles.
+- Sketch a plot of the $\Delta{P}$ as a function of the fluid velocity, clearly indicating the minimum fluidisation velocity.
+- How would your result change if particles were twice as large?
+- What does the terminal falling velocity represent in the context of fluidisation? 
